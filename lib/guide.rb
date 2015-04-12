9f12f27db5ac499dc612de754a8e360cd52e5999
Lisyk <sergey.liskovoy@gmail.com>
2015-04-11 21:05:53 -0400

    add some improvements for representing

--- lib/guide.rb
+++ lib/guide.rb
@@ -1,6 +1,7 @@
 #we establish in init.rb file the path where the first place for searching any require file 
 
 require 'restaurant'
+require 'support/string_extend'
 
 class Guide
     
@@ -67,14 +68,12 @@ class Guide
 
     def list
         output_action_header("Listing restaurants")
-        restaurant = Restaurant.saved_restaurants
-        restaurant.each do |rest|
-            puts rest.name + "|" + rest.cuisine + "|" + rest.formatted_price
-            end
+        restaurants = Restaurant.saved_restaurants
+        output_restaurant_table(restaurants)
     end
 
     def add
-        puts "\nAdd a restaurant\n\n".upcase
+        output_action_header("Add a restaurant")
         
         restaurant = Restaurant.build_from_questions
         
@@ -97,8 +96,23 @@ class Guide
 
     private
     def output_action_header(text)
-        puts  "\n#{text}\n\n".upcase
+        puts  "\n#{text.upcase.center(60)}\n\n".upcase
         
+    end
+
+    def output_restaurant_table(restaurants=[])
+        print " " + "Name".ljust(30)
+        print " " + "Cuisine".ljust(20)
+        print " " + "Price".rjust(6) + "\n"
+        puts "_" * 60
+        restaurants.each do |rest|
+            line = " " << rest.name.titleize.ljust(30)
+            line << " " + rest.cuisine.titleize.ljust(20)
+            line << " " + rest.formatted_price.titleize.rjust(6)
+            puts line
+            end
+        puts "No listings found" if restaurants.empty?
+        puts "_"*60
     end
     
 end
